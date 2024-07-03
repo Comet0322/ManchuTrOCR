6ce108947f37b506c9803a1be4c134c6c99ee837
Jerry <Comet0322@protonmail.com>
2024-07-04 03:26:22 +0800

    Update prepare_data.sh script to generate OCR data

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python3 data/金剛經/image_generate.py
-python3 data/synth/image_generate.py
+# trdg -l mc -c 1000000 -rbl -b 1 -d2 -rk -f 64 --word_split -t 16 --output_dir data/synth2/images
+python3 data/synth6/image_generate.py
 python3 split_data.py
