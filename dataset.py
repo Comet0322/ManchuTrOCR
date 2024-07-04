78205bf0355377668023dd548b31fd0dcd3be91c
Jerry <Comet0322@protonmail.com>
2024-07-05 05:05:29 +0800

    Fix data augmentation bug and delete unused code

--- dataset.py
+++ dataset.py
@@ -24,7 +24,7 @@ def get_black_channel(image):
     return gray
 
 class ManchuDataset(Dataset):
-    def __init__(self, df, transform, processor, max_len=25):
+    def __init__(self, df, transform, processor, max_len=30):
         if isinstance(df, str):
             self.df = pd.read_csv(df, keep_default_na=False)
         else:
@@ -43,29 +43,26 @@ class ManchuDataset(Dataset):
         # extract black pixels
         black = get_black_channel(img)
         _, img = cv2.threshold(black, 120, 255, cv2.THRESH_BINARY)
-        # invert
         img = cv2.bitwise_not(img)
-        # rotate 90
         img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        # gray to RGB
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 
-
-        img = cv2.copyMakeBorder(
-            img, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=(255, 255, 255)
-        )
         target_height, target_width = 64, self.processor.image_processor.size["width"]
-        h, w, _ = img.shape
-        height = target_height
-        width = int(w * height / h)
-        if width >= target_width:
-            img = cv2.resize(img, (target_width, target_height))
-        else:
-            img = cv2.resize(img, (width, height))
-
         if self.transform is not None:
-            img = self.transform(image=img, target_width=target_width) #["image"]
+            img = self.transform(image=img, target_width=target_width)
         else:
+            pad = int(img.shape[0] * 0.15)
+            img = cv2.copyMakeBorder(
+                img, pad, pad, pad, pad, cv2.BORDER_CONSTANT, value=(255, 255, 255)
+            )
+            h, w, _ = img.shape
+            height = target_height
+            width = int(w * height / h)
+            if width >= target_width:
+                img = cv2.resize(img, (target_width, target_height))
+            else:
+                img = cv2.resize(img, (width, height))
+
             img_pad = np.zeros((target_height, target_width, 3), dtype=img.dtype)
             img_pad.fill(255)
             img_pad[:height, :width] = img
@@ -97,14 +94,7 @@ class ManchuDataset(Dataset):
 
         return encoding
 
-    def select(self, indices: Union[List[int], torch.Tensor]) -> "ManchuDataset":
-        return ManchuDataset(
-            self.df.iloc[indices].reset_index(drop=True),
-            self.transform,
-            self.processor,
-            self.max_len,
-        )
-
+        
 
 def collate_fn(batch):
     pixel_values = torch.stack([item["pixel_values"] for item in batch])
@@ -115,88 +105,23 @@ def collate_fn(batch):
         return {"pixel_values": pixel_values}
 
 
-def aug(image, target_width=480, **kwargs):
-    # # morphological alterations
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    if random.randint(1, 5) == 1:
-        # dilation because the image is not inverted
-        image = cv2.erode(image, kernel, iterations=random.randint(1, 2))
-    if random.randint(1, 5) == 1:
-        # erosion because the image is not inverted
-        image = cv2.dilate(image, kernel, iterations=1)
+def aug2(image, target_width=480, **kwargs):
     image = Image.fromarray(image)
     random_rotate = random.uniform(-2, 2)
-    rotated_image = image.rotate(random_rotate, resample=Image.BICUBIC, expand=True, fillcolor=(255, 255, 255))
-    rotated_image = np.array(rotated_image)
-    rotated_size = (rotated_image.shape[0], rotated_image.shape[1])
-
-    random_scale_x = random.uniform(0.7, 1)
-    random_scale_y = random.uniform(0.7, 1)
+    image = image.rotate(random_rotate, resample=Image.BICUBIC, expand=True, fillcolor=(255, 255, 255))
+    image = np.array(image)
+    
+    random_scale_x = random.uniform(0.7, 1.2)
+    random_scale_y = random.uniform(0.7, 1.2)
     new_size = (
-        int(rotated_size[1] * random_scale_x),
-        int(rotated_size[0] * random_scale_y),
+        int(image.shape[1] * random_scale_x),
+        int(image.shape[0] * random_scale_y),
     )
-    resized_image = cv2.resize(rotated_image, new_size, interpolation=cv2.INTER_AREA)
-
-    random_padding_top = random.randint(0, 10)
-    random_padding_bottom = random.randint(0, 10)
-    random_padding_left = random.randint(0, 10)
-    random_padding_right = random.randint(0, 10)
-
-    padded_image = cv2.copyMakeBorder(
-        resized_image,
-        random_padding_top,
-        random_padding_bottom,
-        random_padding_left,
-        random_padding_right,
-        cv2.BORDER_CONSTANT,
-        value=(255, 255, 255),
+    image = cv2.resize(image, new_size, interpolation=cv2.INTER_AREA)
+    pad = int(image.shape[0] * 0.15)
+    image = cv2.copyMakeBorder(
+        image, pad, pad, pad, pad, cv2.BORDER_CONSTANT, value=(255, 255, 255)
     )
-    padded_size = (padded_image.shape[0], padded_image.shape[1])
-
-    # random_trim_top = int(random.uniform(2, 8))
-    # random_trim_bottom = int(random.uniform(2, 8))
-
-    # trimmed_image = padded_image[
-    #     int(random_trim_top) : padded_size[0] - int(random_trim_bottom),
-    #     0 : padded_size[1],
-    # ]
-
-    # trimmed_size = (trimmed_image.shape[0], trimmed_image.shape[1])
-    semi_final_image = cv2.resize(
-        padded_image,
-        (int(padded_size[1] * 64 / padded_size[0]), 64),
-        interpolation=cv2.INTER_AREA,
-    )
-
-    if semi_final_image.shape[1] <= target_width:
-        # random offset
-        random_offset = random.randint(0, target_width - semi_final_image.shape[1])
-        final_image = cv2.copyMakeBorder(
-            semi_final_image,
-            0,
-            0,
-            random_offset,
-            target_width - semi_final_image.shape[1] - random_offset,
-            cv2.BORDER_CONSTANT,
-            value=(255, 255, 255),
-        )
-    else:
-        # resize
-        final_image = cv2.resize(
-            semi_final_image, (target_width, 64), interpolation=cv2.INTER_AREA
-        )
-
-    gauss = np.random.normal(0, 1, final_image.size)
-    gauss = gauss.reshape(final_image.shape[0], final_image.shape[1], 3).astype("uint8")
-    noise = final_image + 0.2 * gauss
-    # int8
-    noise = noise.astype("uint8")
-
-    return noise
-
-
-def aug2(image, target_width=480, **kwargs):
     # only augment 3/4th the images
     if image.shape[1] <= target_width:
         random_offset = random.randint(0, target_width - image.shape[1])
@@ -221,9 +146,6 @@ def aug2(image, target_width=480, **kwargs):
     # # morphological alterations
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
     if random.randint(1, 5) == 1:
-        # dilation because the image is not inverted
-        image = cv2.erode(image, kernel, iterations=random.randint(1, 2))
-    if random.randint(1, 5) == 1:
         # erosion because the image is not inverted
         image = cv2.dilate(image, kernel, iterations=1)
 
@@ -243,7 +165,7 @@ def aug2(image, target_width=480, **kwargs):
                                 rain_type="drizzle",
                                 p=0.05,
                             ),
-                            A.RandomShadow(p=1),
+                            # A.RandomShadow(p=1),
                             A.PixelDropout(p=1),
                         ],
                         p=0.9,
@@ -267,27 +189,7 @@ def aug2(image, target_width=480, **kwargs):
                 ],
                 p=1,
             ),
-            # transformations
-            A.OneOf(
-                [
-                    A.ShiftScaleRotate(
-                        shift_limit=0,
-                        scale_limit=(-0.1, 0),
-                        rotate_limit=1,
-                        border_mode=cv2.BORDER_CONSTANT,
-                        value=(255, 255, 255),
-                        p=1,
-                    ),
-                    A.Affine(
-                        shear=random.randint(-5, 5),
-                        mode=cv2.BORDER_CONSTANT,
-                        cval=(255, 255, 255),
-                        p=1,
-                    ),
-                ],
-                p=0.5,
-            ),
-            A.Blur(blur_limit=5, p=0.25),
+            A.Blur(blur_limit=3, p=0.25),
         ]
     )
 
@@ -320,9 +222,8 @@ def get_dataset(
 
 if __name__ == "__main__":
     from transformers import TrOCRProcessor
-
+    random.seed(999)
     from models import get_crnn_model
     model, processor = get_crnn_model(device="cuda")
-    # processor = TrOCRProcessor.from_pretrained("manchu")
-    dataset = get_dataset(processor, "data/train.csv", "data/val.csv", "data/test.csv")
-    print(dataset["train"][0].keys())
+    dataset = get_dataset(processor, "data/train.csv", "data/val.csv", "data/親征平定朔漠方略/crop.csv")
+    print(dataset["train"][0])
